f9af2130e6f0437bb9730197f1ab6602388b0f04
FLMarimoto <filipe.marimoto@fatec.sp.gov.br>
2019-12-23 16:40:53 -0300

    Class 49

--- src/App.js
+++ src/App.js
@@ -34,11 +34,22 @@ class App extends Component {
   }
   
   render() {
+    const style = {
+      backgroundColor: 'white', 
+      font: 'inherit',
+      color: 'blue', 
+      border: '1px solid blue',
+      padding: '8px', 
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-          <button onClick={() => this.switchNameHandler('Max')}>Switch Name</button> {/*LESS EFFICIENT*/}
+          <button 
+            style={style}
+            onClick={() => this.switchNameHandler('Max')}>Switch Name</button> {/*LESS EFFICIENT*/}
         <Person
           name= { this.state.persons[0].name } 
           age = { this.state.persons[0].age }>My Hobbies: Reading</Person>
