59edae2df7499cf74f307e315914cc2fe5ef9245
FLMarimoto <filipe.marimoto@fatec.sp.gov.br>
2019-12-23 14:09:46 -0300

    Class 37

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Person from './Person/Person';
+import person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name="Filipe" age ="33">My Hobbies: Reading</Person>
+        <Person name="Max" age ="37" />
+        <Person name="Jane" age ="23" />
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work??'));
   }
 }
 
