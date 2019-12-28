8d56cb47cc4e76bab1d03e37071c1e9dcfcc9daa
FLMarimoto <filipe.marimoto@fatec.sp.gov.br>
2019-12-28 12:42:49 -0300

    class 90

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: 'qwdas', name: 'Filipe', age: 33 },
@@ -14,6 +19,15 @@ class App extends Component {
     ], 
     otherState: 'Some other value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
   }
 
   nameChangedHandler = (event, id) => {
@@ -43,7 +57,7 @@ class App extends Component {
   }
   
   render() {
-    
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -57,6 +71,7 @@ class App extends Component {
       <div className={classes.App}>
         <Cockpit 
           clicked = {this.togglePersonsHandler}
+          title = {this.props.appTitle}
           showPersons = {this.state.showPersons}
           persons = {this.state.persons}/>
         {persons}
