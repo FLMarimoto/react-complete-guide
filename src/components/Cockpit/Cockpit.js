7c3a6c85fbafe48b1c6dae196a5959579018c0e0
FLMarimoto <filipe.marimoto@fatec.sp.gov.br>
2019-12-30 08:27:14 -0300

    class 97

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -8,11 +8,10 @@ const cockpit = (props) => {
     
     useEffect(() => {
         console.log('[Cockpit.js] useEffect')
-        const timer = setTimeout(() => {
+        setTimeout(() => {
             alert('Saved data to cloud!');
         }, 1000);
         return () => {
-            clearTimeout(timer);
             console.log('[Cockpit.js] cleanup in useEffect')
         };
     }, []);
